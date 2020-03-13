dc6ca14809f93ca90afca0e129bb90d5ef3aa5e8
scrumpygeo <scrumpygeo@yahoo.ca>
2020-03-13 23:04:24 +0000

    send data to child component

--- src/components/Place.js
+++ src/components/Place.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 
 export class Place extends Component {
   render() {
-    return <div>Place</div>;
+    const style = {
+      backgroundImage: `url('${this.props.place.loc_image}')`
+    };
+
+    return (
+      <div className='place' onClick={this.handleClick}>
+        <div className='place-picture' style={style}>
+          {this.props.place.city}
+        </div>
+      </div>
+    );
   }
 }
 
