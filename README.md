8f632ffcda14098cbf7699eabc9accf3d15865e2
scrumpygeo <scrumpygeo@yahoo.ca>
2020-03-13 22:50:30 +0000

    use app as class component

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Basic exercise in using React with Mapbox: react-map-gl.
 Uses json with data relating to some past ERP consulting locations.
 
 Idea is user clicks on image in panel and map on right centres on site location.
+
+Using Visual Studio Code.
--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
 import React, { Component } from 'react';
+import ReactMapGL from 'react-map-gl';
 import './App.css';
 
-function App() {
-  return <div className='App'>Hello there.</div>;
+class App extends Component {
+  state = {
+    viewport: {
+      width: '40vw',
+      height: '100vh',
+      latitude: 55.7803,
+      longitude: -4.0629,
+      zoom: 10
+    }
+  };
+  render() {
+    return (
+      <div className='res-container'>
+        <div className='main'>
+          <div className='cards'></div>
+        </div>
+        <div className='mapContainer'></div>
+      </div>
+    );
+  }
 }
 
 export default App;
