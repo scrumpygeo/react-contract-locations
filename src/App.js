7153b0874960fa7e5424851e0e0ee6c944122513
scrumpygeo <scrumpygeo@yahoo.ca>
2020-03-19 11:50:48 +0000

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import Place from './components/Place';
+import Navbar from './components/Navbar';
 import * as locationsData from './data/locations.json';
 import './App.css';
 
@@ -83,58 +84,61 @@ class App extends Component {
 
   render() {
     return (
-      <div className='res-container'>
-        <div className='cards'>
-          {locationsData.default.map(place => (
-            <Place
-              place={place}
-              selectPlace={this.selectPlace}
-              key={place.id}
-            />
-          ))}
-        </div>
+      <div>
+        <Navbar className='navbar' />
+        <div className='res-container'>
+          <div className='cards'>
+            {locationsData.default.map(place => (
+              <Place
+                place={place}
+                selectPlace={this.selectPlace}
+                key={place.id}
+              />
+            ))}
+          </div>
 
-        <div className='mapContainer' ref={ref => (this.newLocation = ref)}>
-          <ReactMapGL
-            {...this.state.viewport}
-            mapStyle='mapbox://styles/mapbox/outdoors-v11'
-            mapboxApiAccessToken='pk.eyJ1IjoicHZhbDEiLCJhIjoiY2s3bm05dnhnMDA0NDNmcGF2cHpqNXFydCJ9.yA2nGLMoYF3NmD7sSSUI-w'
-            onViewportChange={viewport => this.setState({ viewport })}
-          >
-            {Object.keys(this.state.targetLocation).length !== 0 ? (
-              <Marker
-                latitude={this.state.targetLocation.lat}
-                longitude={this.state.targetLocation.lng}
-              >
-                <button
-                  className='marker-btn'
-                  onClick={e => {
-                    this.setSelectedPlace(this.state.targetLocation);
-                  }}
+          <div className='mapContainer' ref={ref => (this.newLocation = ref)}>
+            <ReactMapGL
+              {...this.state.viewport}
+              mapStyle='mapbox://styles/mapbox/outdoors-v11'
+              mapboxApiAccessToken='pk.eyJ1IjoicHZhbDEiLCJhIjoiY2s3bm05dnhnMDA0NDNmcGF2cHpqNXFydCJ9.yA2nGLMoYF3NmD7sSSUI-w'
+              onViewportChange={viewport => this.setState({ viewport })}
+            >
+              {Object.keys(this.state.targetLocation).length !== 0 ? (
+                <Marker
+                  latitude={this.state.targetLocation.lat}
+                  longitude={this.state.targetLocation.lng}
                 >
-                  <img src='location-icon.svg' className='pin' alt='pin' />
-                </button>
-              </Marker>
-            ) : (
-              <div></div>
-            )}
+                  <button
+                    className='marker-btn'
+                    onClick={e => {
+                      this.setSelectedPlace(this.state.targetLocation);
+                    }}
+                  >
+                    <img src='location-icon.svg' className='pin' alt='pin' />
+                  </button>
+                </Marker>
+              ) : (
+                <div></div>
+              )}
 
-            {Object.keys(this.state.selectedSite).length !== 0 &&
-            this.state.showPopup ? (
-              <Popup
-                longitude={this.state.selectedSite.lng}
-                latitude={this.state.selectedSite.lat}
-                closeButton={true}
-                closeOnClick={true}
-                onClose={() => this.setState({ showPopup: false })}
-              >
-                <div className='popup'>
-                  <h3>{`${this.state.selectedSite.company} `}</h3>
-                  <p>{`${this.state.selectedSite.skillset} `}</p>
-                </div>
-              </Popup>
-            ) : null}
-          </ReactMapGL>
+              {Object.keys(this.state.selectedSite).length !== 0 &&
+              this.state.showPopup ? (
+                <Popup
+                  longitude={this.state.selectedSite.lng}
+                  latitude={this.state.selectedSite.lat}
+                  closeButton={true}
+                  closeOnClick={true}
+                  onClose={() => this.setState({ showPopup: false })}
+                >
+                  <div className='popup'>
+                    <h3>{`${this.state.selectedSite.company} `}</h3>
+                    <p>{`${this.state.selectedSite.skillset} `}</p>
+                  </div>
+                </Popup>
+              ) : null}
+            </ReactMapGL>
+          </div>
         </div>
       </div>
     );
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navbar.css';
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <h3>List or Former ERP Contract Sites</h3>
+    </div>
+  );
+}
+
+export default Navbar;
